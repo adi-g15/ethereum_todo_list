7472a59eed4d996bcb29fc5af1f7788f70b0c49a
Aditya Gupta <ag15035@gmail.com>
2021-06-09 23:08:48 +0530

    Successfully Connected to MetaMask

--- src/app.js
+++ src/app.js
@@ -1,44 +1,15 @@
 class App {
 	async load() {
-		await this.loadWeb3();
-		await this.loadAccount();
+		// also loads the account
+		await this.enableEthereum();
 	}	
 
-	async loadWeb3() {
-		if(typeof web3 !== 'undefined') {
-			this.web3Provider = web3.currentProvider;
-			web3 = new Web3(web3.currentProvider);
-		} else {
-			alert("Please connect to Metamask");
-		}
+	async enableEthereum() {
+		// Will start the metamask extension
+		const accounts = await window.ethereum.request({method: 'eth_requestAccounts'});
+		const account = accounts[0];
 
-		// Modern dapp browsers...
-		if (window.ethereum) {
-			windows.web3 = new Web3(ethereum);
-			try {
-				// Request account access if needed
-				await ethereum.enable();
-				
-				// Accounts now exposed
-				web3.eth.sendTransaction({})
-			} catch (err) {
-				// User denied account access...
-				alert("You probably Denied access. Can't continue...")
-			}
-		}
-		// Legacy dapp browsers...
-		else if (window.web3) {
-			this.web3Provider = web3.currentProvider;
-			window.web3 = new Web3(web3.currentProvider)
-
-			// Accounts always exposed in legacy
-			web3.eth.sendTransaction({})
-		}
-	}
-
-	async loadAccount () {
-		this.account = web3.eth.accounts[0];
-		console.log("Ethereum Account: ", this.account);
+		console.log("Your Ethereum Account: ", account);
 	}
 }
 
